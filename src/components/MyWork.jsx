1f2aec97cf1f93c797fa03926d078a5346b587df
Rama.ui661 <Ramap.ui661@gmail.com>
2023-09-20 14:58:42 -0500

    Added Visa Poject  Responsibilities to exsisting porfolio

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -7,6 +7,39 @@ export default function MyWork () {
     return (
             <div className="bg-color mtb plr">
                 <h1 className="text-center">Recent Projects</h1>
+                
+                <Card className={'mtb'}>
+                    <Card.Header as="h3" style={{color: 'salmon'}}>CLIENT:VISA <br/>
+                        <span className="text"><b>Location:</b> Austin, TX</span><br/>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Title as="h4" style={{color: 'salmon'}}>Job Role:UI Developer With React Js</Card.Title>
+                        <span className="span-text">Responsibilities: </span><br/>
+                        <Card.Text>
+                            <ul className='text'>
+                                <li>Develop Transaction Management search results within the grid table filter.</li>
+                                <li>Implemented user authentication using Two Factor Authentication feature to secure login of an application.</li>
+                                <li>Worked with Product Platform for Transaction Management</li>
+                                <li>Implement Search features like Search filters, Free-text Search, Advance search, Quick search, Grid Search using search engine criteria.</li>
+                                <li>Implement Save search, reset search and Delete Search buttons with the functionality.</li>
+                                <li>Experience in using React hooks, Redux to manage the application state and applying middleware using redux saga.</li>
+                                <li>Experience in Node environment using Node JS, NVM, NPM, Groovy.</li>
+                                <li>Used Webpack as module bundler or minify code by removing white spaces and line spaces and making all JavaScript files to a single file bundle.js and SASS preprocessing for configuring tasks.</li>
+                                <li>Translated UX design team's wireframes and mockups into responsive, interactive features, using HTML, CSS, JavaScript, and Bootstrap.</li>
+                                <li>Used WCAG (Web Content Accessibility Guidelines) and WCAG 2.0 tools that simplify accessibility to assist people with low vision and disabilities.</li>
+                                <li>Used source-map, CSS loader, babel-loader, SASS loader in web pack which helps in debugging the code in chrome development tools and to convert one file to another file.</li>
+                            </ul>
+                        </Card.Text>
+                        <span className={"text"}><b>Technologies Used:</b> Javascript, React Js, React-Semantic-UI, NodeJs, edge node, SQL, Hive, Hadoop</span>
+                    </Card.Body>
+                    <Card.Footer>
+                            <span className={'text'}> To visit website
+                                <a target ='_blank' rel="noopener noreferrer" href={"https://www.usaa.com/"}>
+                                    <button className={"button"}>Click Here</button>
+                                </a>
+                            </span>
+                    </Card.Footer>
+                </Card>
 
                 <Card className={'mtb'}>
                     <Card.Header as="h3" style={{color: 'salmon'}}>CLIENT:LOWES <br/>
@@ -17,10 +50,17 @@ export default function MyWork () {
                         <span className="span-text">Responsibilities: </span><br/>
                         <Card.Text>
                             <ul className='text'>
-                               <li>Coming soon ...</li>
+                                <li>Working as a Store Analytics in Data Science team</li>
+                                <li>Implementing labour forecast engine </li>
+                                <li>Uses Apache Hadoop exposes services for accessing and manipulating HDFS content with the help of WebHDFS REST APIs </li>
+                                <li>Uses HBase software tool of the Hadoop ecosystem that provides read and write access in real-time for data in the Hadoop file system </li>
+                                <li>Implement Hive(data warehouse software) that allows users to quickly and easily write SQL-like queries to extract data from Hadoop.</li>
+                                <li>Experience working with Apache hive</li>
+                                <li>Implement UI using React Js and developing design using React-Semantic-UI</li>
+                                <li>Implement backend services using NodeJs</li>
                             </ul>
                         </Card.Text>
-                        <span className={"text"}><b>Technologies Used:</b> HTML5, CSS3, SCSS, Javascript, React Js, Bootstrap, React Bootstrap, React Strap</span>
+                        <span className={"text"}><b>Technologies Used:</b> Javascript, React Js, React-Semantic-UI, NodeJs, edge node, SQL, Hive, Hadoop</span>
                     </Card.Body>
                     <Card.Footer>
                             <span className={'text'}> To visit website
